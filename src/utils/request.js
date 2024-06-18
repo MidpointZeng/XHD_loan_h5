fb4ef5039ef513c31f671307a9b1f3ebf44fd62b
DESKTOP-UPBD348\A <zzq>
2024-06-18 09:21:40 +0800

    zzq/2024/3/5/18.1 git commit -mzzq/2024/3/5/18.1

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,11 @@ import store from "@/store";
 import { getToken, removeToken } from "@/utils/auth";
 import axios from "axios";
 import { Toast } from "vant";
+import VueI18n from 'vue-i18n';
+import i18nData from "@/i18n";
+const dgaj=localStorage.getItem('lang')  || 'ZW';
 
+// console.log(dagsa);
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   // baseURL:'http://47.238.168.229:4055/api/',
@@ -17,7 +21,7 @@ service.interceptors.request.use(
   (config) => {
     // 请求前显示加载效果
     toast = Toast.loading({
-      message: "読み込み中...",
+      message:'Read in...',
       forbidClick: true,
       duration: 0,
     });
